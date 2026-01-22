fe1ac7ff01aeb7ba328533724df835624f12503e
franshe22 <franshecamargo22@gmail.com>
2026-01-21 19:43:39 -0500

    Proyecto 7 V2

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Proyecto-7
+# Proyecto-7  
+
+## Objetivos
+
+Desarollar una pequeña aplicacion que incluya graficos, histogramas y graficos de dispercion atravez del desarollo de un script en python. 
+
+## Tecnoligias Utilizadas
+
+- Python
+- Pandas
+- Streamlit
+- Plotly
+- Matplotlib
+- VS Code
+
+## Ejecucion
+
+Tomar el enlace generado y ejecutarlo en un sitio web. 
+
+## Enlaces
+
+Enlace de mi aplicacion
+
---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+     
+st.header('My small app')
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+scatter_button = st.button('construir grafico de dispercion')
+     
+if hist_button: # al hacer clic en el botón
+         # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         
+         # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button:
+    st.write('creacion de un diagrama de dispercion')
+    fig = px.scatter(car_data,x= 'odometer', y = 'price')
+    st.plotly_chart(fig,use_container_width=True) 
